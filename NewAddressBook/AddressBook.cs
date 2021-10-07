07604e4a43ff4324a8821d6d3b19090922fdbf79
vidyasagar gaikwad <vidyasagargaikwad7128@gmail.com>
2021-10-08 01:59:23 +0530

    Added: Edit Contact

--- NewAddressBook/AddressBook.cs
+++ NewAddressBook/AddressBook.cs
@@ -14,24 +14,24 @@ namespace NewAddressBook
 		/// </summary>
 		public void Create()
 		{
-			Console.WriteLine("First Name: ");
+			Console.Write("First Name: ");
 			string firstName = Console.ReadLine();
-			Console.WriteLine("Last Name: ");
+			Console.Write("Last Name: ");
 			string lastName = Console.ReadLine();
 
-			Console.WriteLine("Address: ");
+			Console.Write("Address: ");
 			string address = Console.ReadLine();
-			Console.WriteLine("City: ");
+			Console.Write("City: ");
 			string city = Console.ReadLine();
 
-			Console.WriteLine("State: ");
+			Console.Write("State: ");
 			string state = Console.ReadLine();
-			Console.WriteLine("Zip: ");
+			Console.Write("Zip: ");
 			long zip = long.Parse(Console.ReadLine());
 
-			Console.WriteLine("Phone No: ");
+			Console.Write("Phone No: ");
 			long phone = long.Parse(Console.ReadLine());
-			Console.WriteLine("Email: ");
+			Console.Write("Email: ");
 			string email = Console.ReadLine();
 			Contact contact = new Contact()
 			{
@@ -57,6 +57,57 @@ namespace NewAddressBook
 			}
 		}
 		/// <summary>
+		/// Edit Contact using Name
+		/// </summary>
+		public void editData()
+		{
+			Console.Write("Enter Name which do you want to Edit:");
+			string editName = Console.ReadLine();
+			int index = 0;
+			Console.Write("First Name: ");
+			string firstName = Console.ReadLine();
+			Console.Write("Last Name: ");
+			string lastName = Console.ReadLine();
+
+			Console.Write("Address: ");
+			string address = Console.ReadLine();
+			Console.Write("City: ");
+			string city = Console.ReadLine();
+
+			Console.Write("State: ");
+			string state = Console.ReadLine();
+			Console.Write("Zip: ");
+			long zip = long.Parse(Console.ReadLine());
+
+			Console.Write("Phone No: ");
+			long phone = long.Parse(Console.ReadLine());
+			Console.Write("Email: ");
+			string email = Console.ReadLine();
+			Contact contact = new Contact()
+			{
+				firstName = firstName,
+				lastName = lastName,
+				address = address,
+				city = city,
+				state = state,
+				zip = zip,
+				phone = phone,
+				email = email,
+			};
+			foreach (Contact ct in arrayList)
+			{
+				if (ct.firstName == editName)
+					index = arrayList.IndexOf(ct);
+			}
+			for (int i = 0; i < arrayList.Count; i++)
+			{
+				if (arrayList[i] == arrayList[index])
+				{
+					arrayList[i] = contact;
+				}
+			}
+		}
+		/// <summary>
 		/// Switch for Repeat Process
 		/// </summary>
 		public void repeat()
@@ -64,6 +115,7 @@ namespace NewAddressBook
 			Console.WriteLine("");
 			Console.WriteLine("press 1 to create contact :");
 			Console.WriteLine("press 2 to print contacts :");
+			Console.WriteLine("press 3 to edit contacts :");
 			Console.WriteLine("press 0 to exit program :");
 			int res = int.Parse(Console.ReadLine());
 			switch (res)
@@ -76,7 +128,10 @@ namespace NewAddressBook
 					showAllContacts();
 					repeat();
 					break;
-
+				case 3:
+					editData();
+					repeat();
+					break;
 				case 0:
 					Console.WriteLine("Exit");
 					break;
